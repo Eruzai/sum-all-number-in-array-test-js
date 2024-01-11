cd82f7bcc68f072855137d389eea880bcda847d4
Kevin Levasseur <kevin_levasseur@telus.net>
2024-01-10 18:03:39 -0800

    removed lines of code that were unneeded

--- index.js
+++ index.js
@@ -1,19 +1,15 @@
 function sumItems(array) {
-  if (array.length == 1 && !Array.isArray(array)) {
-    return array[0];
-  }
-
   let sum = 0;
 
   array.forEach((num) => {
     if (Array.isArray(num)) {
       sum += sumItems(num);
     } else {
-    sum += num;
+      sum += num;
     }
   });
 
   return sum;
-};
+}
 
 module.exports = sumItems;
