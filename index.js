5f7b6fb3e3143f982d21d962fea740b85fc7d2ff
Kevin Levasseur <kevin_levasseur@telus.net>
2024-01-10 17:56:17 -0800

    initial commit

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 function sumItems(array) {
-  // Sum all the numbers in the array
-}
+  if (array.length == 1 && !Array.isArray(array)) {
+    return array[0];
+  }
+
+  let sum = 0;
+
+  array.forEach((num) => {
+    if (Array.isArray(num)) {
+      sum += sumItems(num);
+    } else {
+    sum += num;
+    }
+  });
+
+  return sum;
+};
 
 module.exports = sumItems;
